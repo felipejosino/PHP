e73e70395deee834c1b76950268caf239969077b
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-10-31 10:00:09 -0300

    Iniciando PHP
    Primeira aula Curso em Video de PHP

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Moderno</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, ";
+            print "mundo! \u{1F30E}";
+        ?>
+    </h1>
+        <p>Primeira aula Guanabara</p>
+    
+</body>
+</html>
