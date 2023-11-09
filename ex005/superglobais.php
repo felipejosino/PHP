743b56d025cc367d945d90ac766f3287a72f72ec
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-11-08 21:17:51 -0300

    Adicionado mais superglobais

--- ex005/superglobais.php
+++ ex005/superglobais.php
@@ -17,6 +17,10 @@
             * $_FILES       * $GLOBALS
              -->
             <?php 
+                setcookie("dia-da-semana", date("D"), time() + 3600);
+                session_start();
+                $_SESSION["teste"] = "Funcionou!";
+
                 echo "<h1>Superglobal GET</h1>";
                 var_dump($_GET);
                 echo "<br>";
@@ -27,6 +31,26 @@
 
                 echo "<h1>Superglobal REQUEST</h1>";
                 var_dump($_REQUEST);
+                echo "<br>";
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+                echo "<br>";
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+                echo "<br>";
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+                echo "<br>";
+
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+                echo "<br>";
+
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
     </main>
