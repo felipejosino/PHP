590b2a71f5905063e2870c781f5d462271dbc8d2
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-11-10 15:28:20 -0300

    Criado Desafio 10

---
+++ Desafios/Desafio 10/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 10</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        $anoAtual = date("Y");
+
+        $nasc = $_REQUEST['nasc'] ?? '2000';
+        $ano = $_REQUEST['ano'] ?? $anoAtual;
+    ?>
+    <main>
+        <h1>Maquina do Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <label for="nasc">Em que ano você nasceu ?</label>
+            <input type="number" name="nasc" id="idnasc" min="1900" max="<?=$anoAtual?>" value="<?=$nasc?>">
+
+            <label for="ano">Quer saber sua idade em que ano ? <small>Atualmente estamos no ano <?=$anoAtual?></small> </label>
+            <input type="number" name="ano" id="idano" min="1900" value="<?=$ano?>">
+
+            <input type="submit" value="Qual será minha idade?">
+        </form>
+    </main>
+
+    <section>
+        
+        <?php 
+            $idade = $ano - $nasc;
+        ?>
+        <h2>Resultado</h2>
+        <p>Quem nasceu em <?=$nasc?> vai te <strong><?=$idade?> anos</strong> em <?=$ano?>!</p>
+    </section>
+    
+</body>
+</html>
